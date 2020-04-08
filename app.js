8bdf8181209adcbf33a2d88d5c25c82dd1d08b26
Schaplowsky, Amy <Kyle.Schaplowsky@wolterskluwer.com>
2020-04-08 09:52:47 -0500

    reformatted with prettier and added async await

--- app.js
+++ app.js
@@ -6,21 +6,28 @@ const mongoose = require("mongoose");
 const graphQlSchema = require("./graphql/schema");
 const graphQlResolvers = require("./graphql/resolvers");
 
-const app = express();
+async function main() {
+  const app = express();
 
-app.use(bodyParser.json());
+  app.use(bodyParser.json());
 
-app.use("/graphql", graphQlHttp({
-    schema: graphQlSchema,
-    rootValue: graphQlResolvers,
-    graphiql: true
-}));
-
-mongoose
-    .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-uifhw.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
-    .then(() => {
-        app.listen(3000);
+  app.use(
+    "/graphql",
+    graphQlHttp({
+      schema: graphQlSchema,
+      rootValue: graphQlResolvers,
+      graphiql: true,
     })
-    .catch((err) => {
-        throw err;
-    });
+  );
+
+  await mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-uifhw.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  );
+
+  app.listen(3000);
+}
+
+main().catch((err) => {
+  console.error(err);
+  throw err;
+});
